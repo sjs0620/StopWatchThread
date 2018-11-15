42586817cf6e006bcefe8845f24650b2981cda41
sjs0620 <sjslsm@gmail.com>
2018-11-15 14:33:16 +0900

    Handler

--- app/src/main/java/com/example/edu/stopwatchthread/MainActivity.java
+++ app/src/main/java/com/example/edu/stopwatchthread/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.edu.stopwatchthread;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button buttonStart, buttonStop;
-    long endTime = 0;
-    final String TAG = "ErrorThreadActivity Tag";
+    Button buttonStart, buttonPause, buttonReset;
+    TextView textResult;
+    long startTime = 0, timeBuff = 0;
+    Handler  handler;
+    long updateTime=0,millisecondTime=0;
+    int minutes, seconds,milliSeconds;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,33 +24,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         buttonStart = (Button)findViewById(R.id.buttonStart);
-        buttonStop = (Button)findViewById(R.id.buttonStop);
+        buttonPause = (Button)findViewById(R.id.buttonPause);
+        buttonReset = (Button)findViewById(R.id.buttonReset);
+        textResult = (TextView)findViewById(R.id.textViewResult);
+
         buttonStart.setOnClickListener(this);
-        buttonStop.setOnClickListener(this);
+        buttonPause.setOnClickListener(this);
+        buttonReset.setOnClickListener(this);
+        handler = new Handler();
     }
 
     @Override
     public void onClick(View v) {
-        TextView textResult = (TextView)findViewById(R.id.textViewResult);
+
         switch (v.getId()){
             case R.id.buttonStart:
-                endTime = System.currentTimeMillis() + (2*5000);
-                Log.i(TAG, "Thread running!");
-                while (System.currentTimeMillis() < endTime){
-                    synchronized (this){
-                        long currentTime = System.currentTimeMillis();
-                        String strTime = String.valueOf(endTime-currentTime);
-//                        Log.i(TAG,"Thread running!");
-                        Log.i(TAG,strTime);
-//                        textResult.setText(String.valueOf(System.currentTimeMillis()));
-                        textResult.setText(strTime);
-                    }
-                }
+                startTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable,0);
                 break;
-            case R.id.buttonStop:
-                endTime = System.currentTimeMillis();
+            case R.id.buttonPause:
+                timeBuff += millisecondTime;
+                handler.removeCallbacks(runnable);
+                break;
+            case R.id.buttonReset:
+                millisecondTime = 0;
+                timeBuff = 0;
+                startTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable,0);
                 break;
         }
-
     }
+
+    public Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            millisecondTime = SystemClock.uptimeMillis() - startTime;
+            updateTime = timeBuff + millisecondTime;
+            seconds = (int)(updateTime/1000);
+            minutes = seconds/60;
+            seconds = seconds%60;
+            milliSeconds = (int)(updateTime % 1000);
+            textResult.setText(minutes + " : " + String.format("%02d",seconds) +
+                    " : " + String.format("%03d",milliSeconds));
+
+            handler.postDelayed(this,0);
+        }
+    };
+
 }
